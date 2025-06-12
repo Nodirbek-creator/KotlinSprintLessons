17532e01b0006a009bf34fd216f4790284711159
Nodirbek Bakhromov <nodirbekbakhromov08@gmail.com>
2025-06-12 09:05:19 +0500

    KSL-1-2 completed the assignment

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main(){
+    var numberOfOrders: Int = 75
+    val thanksMessage: String = "Спасибо за покупку!"
+    println("$thanksMessage\nКоличество ваших закаказов: $numberOfOrders")
+
+    var numberOfEmployees: Short = 2000
+//    println("Текущее количество работников: $numberOfEmployees")
+
+    numberOfEmployees--
+    println("Текущее количество работников: $numberOfEmployees")
+
+}
